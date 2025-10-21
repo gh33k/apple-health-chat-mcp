8b032821a61efb037cb2e9fb3cc33e6aaa90946d
gh33k <50358886+gh33k@users.noreply.github.com>
2025-10-21 13:59:58 +0300

    Fix Jest deprecation warning - update to modern ts-jest config format
    - Remove deprecated globals config
    - Use modern transform format for ts-jest
    - Clean up empty test files
    - Tests now run without warnings

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,13 @@
 export default {
   preset: 'ts-jest/presets/default-esm',
   extensionsToTreatAsEsm: ['.ts'],
-  globals: {
-    'ts-jest': {
-      useESM: true
-    }
-  },
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
   testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
   transform: {
-    '^.+\\.ts$': 'ts-jest',
+    '^.+\\.ts$': ['ts-jest', {
+      useESM: true
+    }],
   },
   collectCoverageFrom: [
     'src/**/*.ts',
